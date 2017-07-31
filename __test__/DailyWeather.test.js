c7fcff77a75b7009a0b976a637f8d970d2baf0ab
Travis Rollins <TROLL59910@aol.com>
2017-07-31 16:31:26 -0600

    Write tests for DailyWeather and App component.

--- __test__/DailyWeather.test.js
+++ __test__/DailyWeather.test.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { DailyWeather } from '../lib/Components/DailyWeather'
+import { MockWeatherData } from '../lib/utils/MockWeatherData'
 
 describe('DailyWeather', () => {
   let wrapper;
 
+  const wImage = MockWeatherData.forecast.simpleforecast.forecastday[0].icon
+
+  const Data = {
+    icon: 'sdoifwlfkjas',
+    date: {
+      weekday: 'Monday'
+    },
+    high: {
+      fahrenheit: 23
+    },
+    low: {
+      fahrenheit: 3
+    }
+  }
+    //  weatherImage: MockWeatherData.forecast.simpleforecast.forecastday[0].icon,
+    //  image: `../lib/Assets/weather-condition-icons/${wImage}.svg`,
+    //  day: MockWeatherData.forecast.simpleforecast.forecastday[0].date.weekday,
+    //  high: MockWeatherData.forecast.simpleforecast.forecastday[0].high.fahrenheit,
+    //  low: MockWeatherData.forecast.simpleforecast.forecastday[0].low.fahrenheit,
+  // }
+
   beforeEach(() => {
-    wrapper = <DailyWeather />
+    console.log('Data ', Data)
+
+    wrapper = shallow(<DailyWeather weatherdata={Data} />)
+
   })
 
   it('should exist', () => {
-    console.log(shallow(<DailyWeather />))
-    expect(wrapper).toBeDefined()
+    expect(true)
+    expect(wrapper).toBeDefined();
   })
 })
